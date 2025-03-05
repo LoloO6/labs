1ea175e8cfc01044aa19c269c2e1f696a14d1703
LoloO6 <ml12365@auca.kg>
2025-03-05 18:26:18 +0600

    labs

--- lab05/hello_world.py
+++ lab05/hello_world.py
@@ -1,18 +1,2 @@
-name = "John"
-print("Name:", name)
-
-a=10
-b=3
-print("Addition:", a+b)
-print("Subtraction:", a-b)
-print("Multiplication:", a*b)
-print("Division:", a/b)
-
-x, y = 5, 10
-x, y = y, x
-print("After swapping: x =", x, ", y =", y)
-
-PI = 3.14159
-radius = 5
-area = PI * (radius**2)
-print("Area of circle:", area)
+#This is a comment explaining the program
+print("Hello, World!") #Output a welcome message
